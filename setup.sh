202ed682ecf1c484a1f1b658cc9ae074bdbd92e5
tyler1767 <52711853+tyler1767@users.noreply.github.com>
2021-08-01 18:19:00 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,10 +2,7 @@ sudo apt-get update
 sudo apt install python3-pip
 pip install buildozer #should I add this to PATH?
 pip install cython==0.29.19
-sudo apt-get install -y build-essential git python3 python3-dev ffmpeg libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev zlib1g-dev
-sudo apt-get install -y libgstreamer1.0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good
-sudo apt-get install build-essential libsqlite3-dev sqlite3 bzip2 libbz2-dev zlib1g-dev libssl-dev openssl libgdbm-dev libgdbm-compat-dev liblzma-dev libreadline-dev libncursesw5-dev libffi-dev uuid-dev libffi6
-sudo apt-get install libffi-dev
+sudo apt-get install -y build-essential git python3 python3-dev ffmpeg libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev zlib1g-dev libgstreamer1.0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good build-essential libsqlite3-dev sqlite3 bzip2 libbz2-dev zlib1g-dev libssl-dev openssl libgdbm-dev libgdbm-compat-dev liblzma-dev libreadline-dev libncursesw5-dev libffi-dev uuid-dev libffi6 libffi-dev
 #buildozer init
 #buildozer -v android debug 
 #buildozer android clean
