5006b100a75bbed90ac069de81556f19fe9790fe
tyler1767 <52711853+tyler1767@users.noreply.github.com>
2021-08-01 19:33:10 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ sudo pip install cython==0.29.19
 sudo apt-get install -y build-essential git python3 python3-dev ffmpeg libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev zlib1g-dev libgstreamer1.0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good build-essential libsqlite3-dev sqlite3 bzip2 libbz2-dev zlib1g-dev libssl-dev openssl libgdbm-dev libgdbm-compat-dev liblzma-dev libreadline-dev libncursesw5-dev libffi-dev uuid-dev libffi-dev sudo apt-get install openjdk-8-jdk cmake automake libltdl-dev
 git clone https://github.com/tyler1767/BeyondBasicBudget.git
 ##need login and personal access token
-cd BeyondBasicBudget
+cd BeyondBasicBudget/src/mobile
 #buildozer init
 #buildozer -v android debug 
 #buildozer android clean
